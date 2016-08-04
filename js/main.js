c0288f1eda887dd6a6372378250046092421cd1d
cindychao <cchao@cchao-4787.marketo.net>
2016-08-04 12:10:24 -0700

    responsive things

--- js/main.js
+++ js/main.js
@@ -17,3 +17,10 @@ $('.trigger').on('click', function () {
 $('.close-modal').on('click', function () {
 	$('.modal').removeClass('show-modal');
 })
+
+// $('.hamburger').on('click', function(e) {
+//   // Prevent link from jumping to the top of the page
+//   e.preventDefault();
+//   // If menu is already showing, slide it up. Otherwise, slide it down.
+//   $('.menu').toggleClass('slide-down');
+// });
